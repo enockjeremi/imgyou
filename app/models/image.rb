09a2c7dd9d112074e183dc24bbe5a1f1c5e7d777
Enock Jeremi <enockjeremi@gmail.com>
2016-07-18 12:59:02 -0400

    Form new
    Añadido el metodo post para el envio de datos a la base de datos
    	- new.html.erb: Contiene el html para el formulario.
    	- es.activerecord.yml: Traducciones para dicho formulario, usando un modelo como base.
    	- application_helper.rb: Creado un helper para que los tags se guarden en la base de datos de manera correcta.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,10 @@
 class Image < ApplicationRecord
-  enum catengory: %w(portrait landscape city nature animals)
+  enum category: %w(portrait landscape city nature animals)
+
+  def tags_text
+    self.tags.join(', ')
+  end
+  def tags_text=(value)
+    self.tags = value.split(',').map { |tag| tag.strip }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/images' => 'images#index'
+  #get '/images' => 'images#index'
+  #get '/images/new' => 'images#new', as: :new_image
+  resources :images, only: [:index, :new, :create]
 end
